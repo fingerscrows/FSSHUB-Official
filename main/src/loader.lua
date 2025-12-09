691e1045d7c228da47b4637802fef026a5ef19bb
fingerscrows <115174083+fingerscrows@users.noreply.github.com>
2025-12-09 18:38:27 +0700

    Update loader.lua

--- main/src/loader.lua
+++ main/src/loader.lua
@@ -2,30 +2,24 @@
 
 if not game:IsLoaded() then game.Loaded:Wait() end
 
--- Hapus 'refs/heads/' dari URL agar lebih stabil di raw github
 local CORE_URL = "https://raw.githubusercontent.com/fingerscrows/fsshub-official/main/main/src/Core.lua"
 
 local success, err = pcall(function()
-    -- Ambil script sebagai chunk
     local coreScript = game:HttpGet(CORE_URL)
     local coreFunc = loadstring(coreScript)
-    
-    -- Jalankan chunk untuk mendapatkan Module Table (Core)
     local CoreModule = coreFunc()
     
-    -- Panggil fungsi Init() dari Core
     if CoreModule and CoreModule.Init then
         CoreModule.Init()
     else
-        warn("[FSSHUB] Core loaded but no Init function found!")
+        warn("[FSSHUB] Core Init not found!")
     end
 end)
 
 if not success then
     game.StarterGui:SetCore("SendNotification", {
         Title = "FSSHUB Error",
-        Text = "Failed to run CORE: " .. tostring(err),
+        Text = "Loader Failed: " .. tostring(err),
         Duration = 5
     })
-    warn("[FSSHUB] Loader Error: " .. tostring(err))
 end
